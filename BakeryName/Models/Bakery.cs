c697b9afb19ebf406cc851c99988cb5ef27f2f37
Amber W <amoothenielson@gmail.com>
2022-02-12 15:24:59 -0800

    Simplify code math for bakery prices.

--- BakeryName/Models/Bakery.cs
+++ BakeryName/Models/Bakery.cs
@@ -1,10 +1,32 @@
 using System.Collections.Generic;
 using System;
-
 using Bakery.Logic;
 
 namespace BakeryFun
 {
   public class Bread
+  {
+    public int LoafAmount { get; set;}
+    public int LoafCost {get; set;}
+
+    public Bread(int numberOfLoaves)
+    {
+      NumberOfLoaves = numberOfLoaves)
+      LoafCost = this.CalculateTotal();
+    }
+    public int CalculateTotal()
+    {
+      if (NumberOfLoaves >= 3)
+      {
+        int discountLoaves = (NumberOfLoaves - (NumberOfLoaves / 3));
+        return discountLoaves *5;
+      }
+      else
+      {
+        return NumberOfLoaves *5;
+      }
+    }
+  }
+}
 
 
