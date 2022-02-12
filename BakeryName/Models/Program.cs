bc51688e5243bed89bbc15f2bf8e58a29e5379b9
Amber W <amoothenielson@gmail.com>
2022-02-11 23:19:15 -0800

    Add writeline code to give prices and deals for bakery prices.

--- BakeryName/Models/Program.cs
+++ BakeryName/Models/Program.cs
@@ -6,9 +6,14 @@ namespace ProgramSpace
 {
   public class Program
   {
-    public static void Main()
+    static void Main()
     {
-      Console.WriteLine("Welcome to Ella's Bakery! Are you ready to order? 'Y' for yes or enter 'N' for no.");
+      Console.WriteLine("Welcome to Ella's Bakery! We've got fresh bread and pastries for sale today!");
+      Console.WriteLine("One loaf is bread is $5, or get two for $10 and get one free!");
+      Console.WriteLine("Or try one of our delicious pastires!");
+      Console.WriteLine("One for $2 or 3 for $5!");
+      Console.WriteLine("How many loaves for ya?");
+      int loaves = int.Parse(Console.ReadLine());
     }
   }
   }
