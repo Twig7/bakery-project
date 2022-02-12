af2fed3cbc162a68acfe82d5cbab13068d725f8b
Amber W <amoothenielson@gmail.com>
2022-02-12 15:29:33 -0800

    Get brain mush from trying to do simple math, try to keep things as simply as possible.

--- BakeryName/Models/pastry.cs
+++ BakeryName/Models/pastry.cs
@@ -2,7 +2,30 @@ using System;
 using System.Collections.Generic;
 using Bakery.Logic;
 
-namespace PastryFun 
+namespace BakeryFun 
 {
   public class Pastry
+  {
+    public int NumberOfPastries { get; set;}
+    public int PastryCost {get; set;}
+
+    public Pastry(int numberOfPastries)
+    {
+      NumberOfPastries = numberOfPastries;
+      PastryCost = this.CalculateTotal();
+    }
+    public int CalculateTotal()
+    {
+      if (NumberOfPastries >= 3)
+      {
+        int discountPastry = NumberOfPastries / 3;
+        int leftovers = NumberOfPastries - discountPastry;
+        return discountPastry * 1 + leftovers * 2;
+      }
+      else
+      {
+        return NumberOfPastries *2;
+      }
+    }
+  }
   
