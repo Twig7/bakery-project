541808b9ac6dfc93a4bd69b0b13331e0653fde60
Amber W <amoothenielson@gmail.com>
2022-02-11 23:25:47 -0800

    Finish console dialogue, add new names for upcoming functions, think about how you forgot to write tests.

--- BakeryName/Models/Program.cs
+++ BakeryName/Models/Program.cs
@@ -14,6 +14,10 @@ namespace ProgramSpace
       Console.WriteLine("One for $2 or 3 for $5!");
       Console.WriteLine("How many loaves for ya?");
       int loaves = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Dang, {loaves} loaves, huh? ...you sure? Ok, ok, what about pastries? How many do you want?");
+      int pastries = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Ok, so that'll be {loaves} loaves and {pastries} pastries!")
+      Console.WriteLine($"Your total today is ${loafOrder.OrderTotal() +pastrieOrder.OrderTotal()} Don't say I didn't warn you!");
     }
   }
   }
